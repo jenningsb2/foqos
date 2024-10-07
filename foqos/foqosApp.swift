4565221e719eb983682f484fe6161fec1e3618a5
Ali Waseem <waseema393@gmail.com>
2024-10-06 18:32:41 -0600

    Initial Commit

---
+++ foqos/foqosApp.swift
@@ -0,0 +1,17 @@
+//
+//  foqosApp.swift
+//  foqos
+//
+//  Created by Ali Waseem on 2024-10-06.
+//
+
+import SwiftUI
+
+@main
+struct foqosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
