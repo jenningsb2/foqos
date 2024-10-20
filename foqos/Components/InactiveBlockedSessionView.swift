af3213c97c953dab7ab49aaebca049b6bb86bf19
Ali Waseem <waseema393@gmail.com>
2024-10-20 16:56:25 -0600

    updated empty screen view

--- foqos/Components/InactiveBlockedSessionView.swift
+++ foqos/Components/InactiveBlockedSessionView.swift
@@ -4,36 +4,31 @@ struct InactiveBlockedSessionView: View {
     let sessions: [BlockedSession]
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Recent Sessions")
-                    .font(.headline)
-                    .fontWeight(.regular)
-                    .foregroundColor(.secondary)
-                
-                List {
-                    ForEach(sessions) { session in
+        Group {
+            if sessions.isEmpty {
+                EmptyView(iconName: "checklist.unchecked", headingText: "No recent sessions to display")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            } else {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Recent Sessions")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(.secondary)
+                    
+                    List(sessions) { session in
                         InactiveBlockedSessionRow(session: session)
                             .listRowSeparator(.hidden)
-                            .listRowInsets(
-                                EdgeInsets(
-                                    top: 8,
-                                    leading: 0,
-                                    bottom: 8,
-                                    trailing: 0
-                                )
-                            )
+                            .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
                     }
+                    .listStyle(PlainListStyle())
                 }
-                .listStyle(PlainListStyle())
             }
         }
     }
-
 }
 
 #Preview {
     let session = BlockedSession(tag: "Work")
     session.endSession()
-    return InactiveBlockedSessionView(sessions: [session])
+    return InactiveBlockedSessionView(sessions: [])
 }
